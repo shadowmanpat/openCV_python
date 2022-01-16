7cff6561e5b61cb2a2772e08ee9a05032b0050da
nagas <nick@nickagas.com>
2022-01-16 08:13:53 +0200

    read video

--- read.py
+++ read.py
@@ -2,6 +2,18 @@ import cv2 as cv
 
 img = cv.imread('Resources/Photos/cat.jpg')
 
-cv.imshow('Cat', img)
+# cv.imshow('Cat', img)
+capture = cv.VideoCapture('Resources/Videos/dog.mp4')
 
-cv.waitKey(0)
+while True:
+    isTrue, frame = capture.read()
+
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+
+cv.destroyAllWindows()
+# cv.waitKey(0)
