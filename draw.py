44573537f922299fdfb457289087650207fcec9f
nagas <nick@nickagas.com>
2022-01-16 09:04:03 +0200

    draw image

--- draw.py
+++ draw.py
@@ -2,9 +2,18 @@ import cv2 as cv
 import numpy as np
 
 blank = np.zeros((500, 500, 3), dtype='uint8')
-cv.imshow('Blank',blank)
+
 img = cv.imread('Resources/Photos/cat.jpg')
-blank[:] = 0,255,0
-cv.imshow('Green',blank)
+# blank[:] = 0,255,0
+# cv.imshow('Green',blank)
 # cv.imshow('Blank',img)
+cv.rectangle(blank, (0,0), (250,250), (0,255,0), thickness=-2)
+
+
+cv.circle(blank, (250,250), 40, (0,0,250), thickness=3)
+cv.line(blank, (0,0),(250,250),(255,255,255), thickness=3)
+cv.putText(blank,"Hello from python and openCV", (250,250), cv.FONT_HERSHEY_TRIPLEX,1.0, (0,255,0), 2)
+
+# cv.line(blank, (250,250), 40, (0,0,250),(255,255,255), thickness=4)
+cv.imshow('Blank',blank)
 cv.waitKey(0)
